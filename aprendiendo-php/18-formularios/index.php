9c3810e7e58e65021467a4dd9b888735d8eb2f6f
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-09-14 12:38:36 +0200

    form basico

---
+++ aprendiendo-php/18-formularios/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios PHP y HTML</title>
+</head>
+
+<body>
+    <h1>Formulario</h1>
+    <form action="" method="POST">
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="" autofocus="autofocus" />
+        <br />
+        <label for="apellido">Apellido: </label>
+        <input type="text" name="apellido" id="">
+        <br />
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
