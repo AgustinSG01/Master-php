dca5e6f7b2c074cf5c20d37bb7180a83f7cef68f
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-09-22 15:03:30 +0200

    a

---
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -0,0 +1,16 @@
+-- Inserts para categorias
+    INSERT INTO categorias VALUES(null, "Acción");
+    INSERT INTO categorias VALUES(null, "Rol");
+    INSERT INTO categorias VALUES(null, "Deportes");
+
+
+-- Inserts para entradas
+    INSERT INTO posts VALUES(null, 1, 2, "Review Elden Ring", "Que buen juego lpm", CURDATE());
+    INSERT INTO posts VALUES(null, 1, 1, "Review COD: Black Cocks", "Pium Pium Pium", CURDATE());
+    INSERT INTO posts VALUES(null, 1, 3, "Review Fifa", "Goofy ahh game", CURDATE());
+    INSERT INTO posts VALUES(null, 3, 3, "Mario Golf", "Un juego de mario de golf, goty of the year del año", CURDATE());
+    INSERT INTO posts VALUES(null, 3, 2, "Asi sin ti", "Requiescate in pache dijo el etsio", CURDATE());
+    INSERT INTO posts VALUES(null, 3, 1, "Battefield > COD", "No se que poner", CURDATE());
+    INSERT INTO posts VALUES(null, 4, 2, "World of Guarcraft", "Paladin nivel 80 type dude", CURDATE());
+    INSERT INTO posts VALUES(null, 4, 3, "necesidad de velocidad", "broom broom", CURDATE());
+    INSERT INTO posts VALUES(null, 4, 1, "Lies of PI", "Paladin nivel 80 type dude", CURDATE());
