597d8c973df2a2fc7b68261307547e94d5ea2b67
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-09-22 11:39:18 +0200

    select basico

---
+++ aprendiendo-sql/04-dml/select.sql
@@ -0,0 +1,9 @@
+-- Mostrar todos los registros de una tabla
+SELECT email, nombre, apellido FROM usuarios;
+
+-- Mostrar todos los campos
+SELECT * FROM usuarios;
+
+-- Operadores aritmeticos
+SELECT id, email, (id + 7) AS "Operacion" FROM usuarios;
+
